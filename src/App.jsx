c3e9f01c079b5c2a19a947b7ef4021b350093ed1
ilsu-sunal <sunalilsu@gmail.com>
2024-05-23 21:09:16 +0300

    cleaning css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,10 +7,10 @@ function App() {
 
   return (
     <>
-      <div>
+      {/*<div>
         <button onClick={notify}>Notify!</button>
         <ToastContainer />
-      </div>
+  </div>*/}
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
