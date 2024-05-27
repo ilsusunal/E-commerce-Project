0f4cbb67fb1a4b1ea27c363161b8c84715fd762f
ilsu-sunal <sunalilsu@gmail.com>
2024-05-27 18:15:11 +0300

    footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,7 +1,10 @@
 export default function Footer(){
     return(
         <>
-        <div>FOOTER</div>
+        <main className="hidden md:flex md:min-h-14 bg-zinc-500 text-white px-12 py-4">
+            <div>@2024 by İlsu Sunal</div>
+        </main>
+        
         </>
     )
 }
