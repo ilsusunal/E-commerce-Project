cf267e1ec2fd5c047704d2a17863402560efce81
ilsu-sunal <sunalilsu@gmail.com>
2024-06-10 16:59:08 +0300

    styling footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,19 @@
 export default function Footer(){
     return(
         <>
-        <main className="hidden w-full md:flex justify-end md:min-h-14 bg-zinc-500 text-white px-12 py-4">
-            <div>@2024 by İlsu Sunal</div>
+        <main className="hidden w-full md:block text-white">
+            <section className="flex bg-zinc-800">
+                <div>
+                    <h2>PazarYeri</h2>
+                    <p>Who Are We?</p>
+                </div>
+                
+
+            </section>
+            <section className="md:flex justify-end md:min-h-8 bg-zinc-500 text-xs px-12 py-2">
+                <div>@2024 by İlsu Sunal</div>
+            </section>
         </main>
-        
         </>
     )
 }
