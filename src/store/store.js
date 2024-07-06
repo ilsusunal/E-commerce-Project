010bef5b737d0062d0572cbf88eeb78978227ea7
ilsu-sunal <sunalilsu@gmail.com>
2024-07-06 16:03:45 +0300

    import issues solved

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import {  legacy_createStore as createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import { thunk } from 'redux-thunk';
+import { createLogger } from 'redux-logger'; 
 import clientReducer from './reducers/clientReducer';
 import productReducer from './reducers/productReducer';
 import shoppingCartReducer from './reducers/shoppingCartReducer';
+
+const logger = createLogger();
 
 const rootReducer = combineReducers({
   client: clientReducer,
