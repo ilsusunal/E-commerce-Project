4567d7f764a0d85112674c8adac8db381549071a
ilsu-sunal <sunalilsu@gmail.com>
2024-07-07 21:03:04 +0300

    BlogPage update - blogcard created

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import BlogCard from '../components/BlogCard'
 
 function BlogPage() {
   return (
-    <div>BlogPage</div>
+    <>
+    <main className="md:max-w-7xl flex flex-col mx-4">
+        <h1 className="text-baseText text-2xl font-bold my-8">Latest News</h1>
+        <div className=" flex flex-wrap gap-4 justify-center">
+           <BlogCard/>
+        </div>
+        </main>
+    </>
   )
 }
 
